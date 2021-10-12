b0f972090c449aa57db0758ad66104090d3b0ec8
scarletgate <xiaoyuedu10@gmail.com>
2021-10-12 11:23:27 +0000

    [add]showとeditのリンク作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,14 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
   def index
     @books = Book.all
   end
@@ -6,19 +16,15 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
-  
-  def new
-    @book = Book.new
-  end
-
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-  end
-
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to books_path(book.id)
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
-  resources :books
+  get 'books' => 'books#index'
   post 'books' => 'books#create'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  get 'books/:id' => 'books#show', as: 'book'
+  patch 'books/:id' => 'bookos#update', as: 'update_book'
+  get 'books/:id//edit' => 'books#show'
+  get 'books/:id' => 'books#edit'
   root to: 'homes#top'
 end
