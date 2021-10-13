1e49df50a202c51422c7b3f6d84deae6c660caa3
scarletgate <xiaoyuedu10@gmail.com>
2021-10-13 09:37:43 +0000

    [Add]フラッシュメッセージまで実装済み

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,34 +1,44 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: "Book was successfully created."
+    else
+      @books = Book.all
+      render :index
+    end
   end
   
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-  end
-
   def index
+    @book = Book.new
     @books = Book.all
   end
-
+  
   def show
     @book = Book.find(params[:id])
   end
-
+  
   def edit
     @book = Book.find(params[:id])
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to books_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "Book was successfully updated."
+    else
+      redirect_to book_path(@book.id)
+    end
+  end
+  
+  def destroy
+     book = Book.find(params[:id])
+     book.destroy
+     redirect_to books_path
   end
   
   private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
   get 'top' => 'homes#top'
   get 'books' => 'books#index'
   post 'books' => 'books#create'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   get 'books/:id' => 'books#show', as: 'book'
-  patch 'books/:id' => 'bookos#update', as: 'update_book'
-  get 'books/:id//edit' => 'books#show'
-  get 'books/:id' => 'books#edit'
-  root to: 'homes#top'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
